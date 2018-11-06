f1ec859571c0804af8359dc629086e59d07d7d7a
Christian Poitras <christian.poitras@ircm.qc.ca>
2018-11-06 10:41:14 -0500

    Fixes license headers.

--- src/main/java/ca/qc/ircm/lana/user/web/SigninView.java
+++ src/main/java/ca/qc/ircm/lana/user/web/SigninView.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018 Institut de recherches cliniques de Montreal (IRCM)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ca.qc.ircm.lana.user.web;
 
 import com.vaadin.flow.component.Tag;
@@ -17,6 +34,11 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 
 /**
  * Sign in view.
+ *
+ * <p>
+ * A lot of code was copied from Vaadin's starter project and is submitted to Commercial Vaadin
+ * Add-On License version 3.
+ * </p>
  */
 @Route(value = SigninView.VIEW_NAME)
 @Tag("signin-view")
