283c0728df54339a45f570bb8c8205985adafc27
Christian Poitras <christian.poitras@ircm.qc.ca>
2018-11-12 17:22:05 -0500

    Adds transaction management.

--- src/main/java/ca/qc/ircm/lana/Main.java
+++ src/main/java/ca/qc/ircm/lana/Main.java
@@ -19,11 +19,13 @@ package ca.qc.ircm.lana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Starts web application.
  */
 @SpringBootApplication
+@EnableTransactionManagement
 public class Main {
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
