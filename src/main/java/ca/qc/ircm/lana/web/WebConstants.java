c153d16c980dcfc8c8bf652a7da8ee3e2b69bf40
Christian Poitras <christian.poitras@ircm.qc.ca>
2018-11-13 14:51:42 -0500

    Adds Web constants.

---
+++ src/main/java/ca/qc/ircm/lana/web/WebConstants.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2010 Institut de recherches cliniques de Montreal (IRCM)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.qc.ircm.lana.web;
+
+/**
+ * Constants for Web.
+ */
+public class WebConstants {
+  public static final String GENERAL_MESSAGES = "VaadinMessages";
+  public static final String APPLICATION_NAME = "application.name";
+  public static final String TITLE = "title";
+  public static final String THEME = "theme";
+  public static final String PRIMARY = "primary";
+  public static final String TERTIARY = "tertiary";
+  public static final String CONTRAST = "contrast";
+  public static final String SUCCESS = "success";
+  public static final String ERROR = "error";
+  public static final String PLACEHOLDER = "placeholder";
+  public static final String FIELD_NOTIFICATION = "field.notification";
+  public static final String REQUIRED = "required";
+  public static final String INVALID = "invalid";
+  public static final String INVALID_NUMBER = "invalidNumber";
+  public static final String INVALID_INTEGER = "invalidInteger";
+  public static final String INVALID_EMAIL = "invalidEmail";
+  public static final String ALREADY_EXISTS = "alreadyExists";
+  public static final String OUT_OF_RANGE = "outOfRange";
+  public static final String SAVE = "save";
+  public static final String CANCEL = "cancel";
+  public static final String ALL = "all";
+}
