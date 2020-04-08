d78fd7611a3564e9b976c9635bc111a2c2416866
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-04-08 12:47:28 -0400

    Removes duplicated annotations on Main class.

--- src/main/java/ca/qc/ircm/lana/Main.java
+++ src/main/java/ca/qc/ircm/lana/Main.java
@@ -19,15 +19,11 @@ package ca.qc.ircm.lana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Starts web application.
  */
 @SpringBootApplication
-@EnableTransactionManagement
-@EnableCaching
 public class Main {
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
