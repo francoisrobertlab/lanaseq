5dbfaba9fb03099324767f58ee0bb7caeb322223
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-04-23 12:50:57 -0400

    Adds missing migration script.

---
+++ src/migration/scripts/20190104174456_all_users_must_have_a_laboratory.sql
@@ -0,0 +1,45 @@
+--
+-- Copyright (c) 2018 Institut de recherches cliniques de Montreal (IRCM)
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+-- // all users must have a laboratory
+-- Migration SQL that makes the change goes here.
+
+UPDATE laboratory
+SET id = id + 1
+ORDER BY id DESC;
+INSERT INTO laboratory (id,name)
+VALUES ('1', 'Informatics');
+UPDATE user
+SET laboratory_id = 1
+WHERE laboratory_id IS NULL;
+ALTER TABLE user
+CHANGE laboratory_id laboratory_id bigint(20) NOT NULL; 
+
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+ALTER TABLE user
+CHANGE laboratory_id laboratory_id bigint(20); 
+UPDATE user
+SET laboratory_id = NULL
+WHERE laboratory_id = 1;
+DELETE FROM laboratory
+WHERE id = 1;
+UPDATE laboratory
+SET id = id - 1
+ORDER BY id ASC;
---
+++ src/migration/scripts/20190111150042_add_access_control_list_tables.sql
@@ -0,0 +1,65 @@
+--
+-- Copyright (c) 2018 Institut de recherches cliniques de Montreal (IRCM)
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+-- // add access control list tables
+-- Migration SQL that makes the change goes here.
+
+CREATE TABLE acl_sid (
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  principal BOOLEAN NOT NULL,
+  sid VARCHAR(100) NOT NULL,
+  UNIQUE KEY unique_acl_sid (sid, principal)
+);
+CREATE TABLE acl_class (
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  class VARCHAR(100) NOT NULL,
+  UNIQUE KEY unique_acl_class (class)
+);
+CREATE TABLE acl_object_identity (
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  object_id_class BIGINT UNSIGNED NOT NULL,
+  object_id_identity VARCHAR(36) NOT NULL,
+  parent_object BIGINT UNSIGNED,
+  owner_sid BIGINT UNSIGNED,
+  entries_inheriting BOOLEAN NOT NULL,
+  UNIQUE KEY unique_acl_object_identity (object_id_class, object_id_identity),
+  CONSTRAINT acl_object_identity_parent FOREIGN KEY (parent_object) REFERENCES acl_object_identity (id),
+  CONSTRAINT acl_object_identity_class FOREIGN KEY (object_id_class) REFERENCES acl_class (id),
+  CONSTRAINT acl_object_identity_owner FOREIGN KEY (owner_sid) REFERENCES acl_sid (id)
+);
+CREATE TABLE acl_entry (
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  acl_object_identity BIGINT UNSIGNED NOT NULL,
+  ace_order INTEGER NOT NULL,
+  sid BIGINT UNSIGNED NOT NULL,
+  mask INTEGER UNSIGNED NOT NULL,
+  granting BOOLEAN NOT NULL,
+  audit_success BOOLEAN NOT NULL,
+  audit_failure BOOLEAN NOT NULL,
+  UNIQUE KEY unique_acl_entry (acl_object_identity, ace_order),
+  CONSTRAINT acl_entry_object FOREIGN KEY (acl_object_identity) REFERENCES acl_object_identity (id),
+  CONSTRAINT acl_entry_acl FOREIGN KEY (sid) REFERENCES acl_sid (id)
+);
+
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DROP TABLE acl_entry;
+DROP TABLE acl_object_identity;
+DROP TABLE acl_class;
+DROP TABLE acl_sid;
