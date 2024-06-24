0e024712bb6e891f1312de3daf8a1f4fc7d4fbbb
Christian Poitras <christian.poitras@ircm.qc.ca>
2024-06-24 18:42:59 -0400

    Use getTranslation in UsersView.

--- src/test/java/ca/qc/ircm/lanaseq/user/web/UsersViewItTest.java
+++ src/test/java/ca/qc/ircm/lanaseq/user/web/UsersViewItTest.java
@@ -19,6 +19,7 @@ package ca.qc.ircm.lanaseq.user.web;
 
 import static ca.qc.ircm.lanaseq.Constants.APPLICATION_NAME;
 import static ca.qc.ircm.lanaseq.Constants.TITLE;
+import static ca.qc.ircm.lanaseq.SpringConfiguration.messagePrefix;
 import static ca.qc.ircm.lanaseq.user.web.UsersView.VIEW_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -33,6 +34,8 @@ import ca.qc.ircm.lanaseq.web.SigninViewElement;
 import ca.qc.ircm.lanaseq.web.ViewLayoutElement;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 
@@ -42,6 +45,11 @@ import org.springframework.security.test.context.support.WithUserDetails;
 @TestBenchTestAnnotations
 @WithUserDetails("lanaseq@ircm.qc.ca")
 public class UsersViewItTest extends AbstractTestBenchTestCase {
+  private static final String MESSAGE_PREFIX = messagePrefix(UsersView.class);
+  private static final String CONSTANTS_PREFIX = messagePrefix(Constants.class);
+  @Autowired
+  private MessageSource messageSource;
+
   private void open() {
     openView(VIEW_NAME);
   }
@@ -81,8 +89,10 @@ public class UsersViewItTest extends AbstractTestBenchTestCase {
   public void title() throws Throwable {
     open();
 
-    assertEquals(resources(UsersView.class).message(TITLE,
-        resources(Constants.class).message(APPLICATION_NAME)), getDriver().getTitle());
+    String applicationName =
+        messageSource.getMessage(CONSTANTS_PREFIX + APPLICATION_NAME, null, currentLocale());
+    assertEquals(messageSource.getMessage(MESSAGE_PREFIX + TITLE, new Object[] { applicationName },
+        currentLocale()), getDriver().getTitle());
   }
 
   @Test
