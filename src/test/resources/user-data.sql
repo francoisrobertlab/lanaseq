33614418e5af95e05061aa4905d652bdba6527df
Christian Poitras <christian.poitras@ircm.qc.ca>
2024-06-27 11:57:33 -0400

    Removed license headers.

--- src/test/resources/user-data.sql
+++ src/test/resources/user-data.sql
@@ -1,20 +1,3 @@
---
--- Copyright (c) 2018 Institut de recherches cliniques de Montreal (IRCM)
---
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU Affero General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU Affero General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
-
 INSERT INTO users (id,email,name,hashed_password,sign_attempts,last_sign_attempt,active,manager,admin,expired_password,locale,creation_date)
 VALUES ('1', 'lanaseq@ircm.qc.ca', 'LANAseq Administrator', '$2a$10$JU0aj7Cc/7sWVkFXoHbWTuvVWEAwXFT1EhCX4S6Aa9JfSsKqLP8Tu', 1, '2018-12-13 17:40:12', 1, 1, 1, 0, null, '2018-11-20 09:30:00');
 INSERT INTO users (id,email,name,hashed_password,sign_attempts,last_sign_attempt,active,manager,admin,expired_password,locale,creation_date)
