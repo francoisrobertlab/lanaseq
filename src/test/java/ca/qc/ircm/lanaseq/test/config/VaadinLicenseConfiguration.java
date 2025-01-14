d6fec52f09c98e92ce412afb8c8c331b7a4e5f99
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-01-14 18:53:10 -0500

    Improves license validation for Vaadin.

---
+++ src/test/java/ca/qc/ircm/lanaseq/test/config/VaadinLicenseConfiguration.java
@@ -0,0 +1,12 @@
+package ca.qc.ircm.lanaseq.test.config;
+
+import java.util.List;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Vaadin license configuration.
+ */
+@ConfigurationProperties(prefix = VaadinLicenseConfiguration.PREFIX)
+public record VaadinLicenseConfiguration(boolean assume, List<String> paths) {
+  public static final String PREFIX = "vaadin.license";
+}
--- src/test/java/ca/qc/ircm/lanaseq/test/config/VaadinLicenseExecutionListener.java
+++ src/test/java/ca/qc/ircm/lanaseq/test/config/VaadinLicenseExecutionListener.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.text.MessageFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
@@ -20,25 +21,22 @@ import org.springframework.test.context.TestExecutionListener;
 public class VaadinLicenseExecutionListener implements TestExecutionListener, InjectDependencies {
   private static final String LICENSE_ERROR_MESSAGE =
       "License for Vaadin TestBench not found. Skipping test class {0} .";
-  private static final String[] LICENSE_PATHS =
-      new String[] { ".vaadin/offlineKey", ".vaadin/proKey", ".vaadin/userKey",
-          "vaadin.testbench.developer.license", ".vaadin.testbench.developer.license" };
-  private static final String LICENSE_SYSTEM_PROPERTY = "vaadin.testbench.developer.license";
   private static final Class<?>[] TEST_BENCH_CLASSES =
       new Class[] { UIUnitTest.class, TestBenchTestCase.class };
   private static final Logger logger =
       LoggerFactory.getLogger(VaadinLicenseExecutionListener.class);
+  private VaadinLicenseConfiguration configuration;
 
   @Override
   public void beforeTestClass(TestContext testContext) throws Exception {
     injectDependencies(testContext.getApplicationContext());
     if (isTestBenchTest(testContext)) {
-      boolean licenseFileExists = false;
-      for (String licencePath : LICENSE_PATHS) {
+      boolean licenseFileExists = configuration.assume();
+      for (String licencePath : configuration.paths()) {
         licenseFileExists |=
             Files.exists(Paths.get(System.getProperty("user.home")).resolve(licencePath));
       }
-      if (!licenseFileExists && System.getProperty(LICENSE_SYSTEM_PROPERTY) == null) {
+      if (!licenseFileExists) {
         String message =
             MessageFormat.format(LICENSE_ERROR_MESSAGE, testContext.getTestClass().getName());
         logger.info(message);
@@ -54,5 +52,10 @@ public class VaadinLicenseExecutionListener implements TestExecutionListener, In
       isTestBenchClass |= testBenchClass.isAssignableFrom(testContext.getTestClass());
     }
     return isTestBenchClass;
+  }
+
+  @Autowired
+  void setConfiguration(VaadinLicenseConfiguration configuration) {
+    this.configuration = configuration;
   }
 }
