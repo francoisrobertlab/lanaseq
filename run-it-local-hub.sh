9083afb6c64c43f06123d0c6c10b33af8f8d60d4
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-05-13 20:00:02 -0400

    Adds example on how to run integration tests on local hub.

---
+++ run-it-local-hub.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+# To configure Docker, see https://vaadin.com/docs/latest/flow/testing/end-to-end/test-grid
+# or https://github.com/SeleniumHQ/docker-selenium/blob/trunk/docker-compose-v3.yml
+
+mvn verify -Dcom.vaadin.testbench.Parameters.hubHostname=localhost
