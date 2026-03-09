093e6c85f1c1a2779c4698c1d038231117fd5f74
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-03-09 19:01:30 -0400

    Replace @Theme by @StyleSheet
    @Theme was deprecated in Vaadin 25.

--- src/main/java/ca/qc/ircm/lanaseq/AppShell.java
+++ src/main/java/ca/qc/ircm/lanaseq/AppShell.java
@@ -2,16 +2,18 @@ package ca.qc.ircm.lanaseq;
 
 import static ca.qc.ircm.lanaseq.UsedBy.VAADIN;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.shared.communication.PushMode;
-import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * Configures PUSH notifications for Vaadin.
  */
 @Push(PushMode.MANUAL)
-@Theme("ircm")
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet("styles.css")
 @UsedBy(VAADIN)
 public class AppShell implements AppShellConfigurator {
 
